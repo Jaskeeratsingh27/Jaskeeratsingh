8c9aa0da03d7ab2db837c76e1754b135dc889610
Jaskeeratsingh27 <73515924+Jaskeeratsingh27@users.noreply.github.com>
2026-09-21 20:11:15 -0500

    Phase 5: serve PWA manifest with correct MIME type

--- tokentrack/server.js
+++ tokentrack/server.js
@@ -1016,6 +1016,7 @@ function serveStatic(req, res) {
       ".js": "text/javascript; charset=utf-8",
       ".css": "text/css; charset=utf-8",
       ".json": "application/json; charset=utf-8",
+      ".webmanifest": "application/manifest+json; charset=utf-8",
       ".svg": "image/svg+xml"
     };
 
